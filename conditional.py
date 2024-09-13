0109ed2e8c1ba114ec555d4280b734606d1f4dad
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-09-13 10:20:01 -0700

    conditional statements

---
+++ conditional.py
@@ -0,0 +1,66 @@
+# marks = 40
+
+# if statement
+# if marks > 35:
+#     print("You are pass in this subject")
+
+
+# day = "Sunday"
+
+# if day == "Sunday":
+#     print("You can play game")
+
+# elif statement
+
+# day = input("Enter any day: ")
+
+# if day == "Sunday":
+#     print("You can play game")
+
+# elif day == "Friday":
+#     print("You cannot play game")
+
+# elif day == "Wednesday":
+#     print("You have to go univeristy")
+
+
+# else statment
+
+# marks = int(input("Enter your maths marks: "))
+
+# if marks >= 33:
+#     print("You are pass in Maths")
+
+# else:
+#     print("You are failed")
+
+
+# if marks >= 33:
+#     print("D")
+
+# elif marks >= 45:
+#     print("C")
+
+# elif marks >= 65:
+#     print("B")
+
+# elif marks >= 75:
+#     print("B+")
+
+# else:
+#     print("You are failed")
+
+
+my_age = input("Enter your age: ")
+
+if my_age == "10":
+    print("You are child")
+
+elif my_age == "15":
+    print("You are not child")
+
+elif my_age == "30":
+    print("You are old")
+
+else:
+    print("You are not child nor old")
