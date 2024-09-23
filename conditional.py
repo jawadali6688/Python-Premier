8a9668a4af13680bb8bad6ad5fd2a6d97b66b918
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-09-23 11:15:29 -0700

    mutable and immutable data types

--- conditional.py
+++ conditional.py
@@ -51,16 +51,95 @@
 #     print("You are failed")
 
 
-my_age = input("Enter your age: ")
+# my_age = input("Enter your age: ")
 
-if my_age == "10":
-    print("You are child")
+# if my_age == "10":
+#     print("You are child")
 
-elif my_age == "15":
-    print("You are not child")
+# elif my_age == "15":
+#     print("You are not child")
 
-elif my_age == "30":
-    print("You are old")
+# elif my_age == "30":
+#     print("You are old")
 
-else:
-    print("You are not child nor old")
+# else:
+#     print("You are not child nor old")
+
+
+# Logical Operators --> and, or, not
+
+# x = True
+
+# y = False
+
+# print(x and y)
+
+
+# x = True
+
+# y = False
+
+# print(x or y)
+
+
+# x = True
+
+# print(not x)
+
+
+# Simple banking loan system
+
+# marks = 75
+
+# if marks >= 90:
+#     print("A+")
+
+# elif marks >= 85:
+#     print("B+")
+
+# else :
+#     print("F")
+
+# age = int(input("Enter you age: "))
+
+# salary = int(input("Enter you salary: ")) 
+
+# loan_amount = int(input("Enter amount you want to take loan: "))
+
+# your_balance = int(input("Enter you current balance: "))
+
+# if age >= 18 and salary >= 30000 or loan_amount >= 500:
+#     print("Eligble for loan")
+#     print(f"Amount of {loan_amount} are transfered into your account")
+#     your_balance += loan_amount
+#     print(f"Now, your current balance is {your_balance}")
+
+# else :
+#     print("Not Eligible")
+
+
+# Match Statement --> Complex pattern matching --> 3.10
+# Switch statements in other languages
+
+# Advance Python
+
+# day = input("Enter day name: ")
+
+# match day:
+
+#     case "Monday":
+#         print("It's a Programming Day")
+
+#     case "Wednesday":
+#         print("Also a Programming Day")
+
+#     case "Friday":
+#         print("Half Day for Programming")
+
+#     case "Sunday":
+#         print("Weekend")
+
+#     case _:
+#         print("Invalid Day")
+
+
---
+++ data_types.py
@@ -0,0 +1,59 @@
+#  Immutable Data Types
+
+# my_str = "Jawad"
+
+# print(id(my_str))
+# # id()
+
+# # my_str += "Khan"
+# my_str = "Khan"
+
+# print(id(my_str))
+
+
+# str1 = "Jawad"
+
+# str2 = "Jawad"
+
+# print(id(str1))
+# print(id(str2))
+
+# my_tuple = ("Jawad", True, False, None)
+
+# # my_tuple[0] = "Khan"
+# print(my_tuple[0])
+
+# print(my_tuple)
+
+
+# Mutable Data Types
+
+# my_list1 = ["Jawad", True, "Khan"]
+
+# # my_list2 = ["Jawad", True, "Khan"]
+
+# my_list2 = my_list1
+
+# # my_list2.append("IUB")
+# # print(my_list2)
+
+# print(id(my_list1))
+# print(id(my_list2))
+
+
+# my_dict = {
+#     "name": "Jawad",
+#     "roll": "01003"
+#  }
+# print(id(my_dict))
+# my_dict["name"] = "Ahmad"
+
+# print(type(my_dict))
+# print(my_dict["name"])
+# print(my_dict)
+# print(id(my_dict))
+
+
+my_set = {1,2,3,4,4,4,4,5,5,8,4,32,4234,32}
+
+print(my_set)
