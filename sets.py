ee01cca64d06b5eeca9c6830094d15d38698ef8e
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-10-09 05:14:57 -0700

    sets in python

---
+++ sets.py
@@ -0,0 +1,48 @@
+# my_set = {1,2,3,3,3,3,4,4,4,5}
+
+# my_set = {"Jawad", "Khan", "Jawad"}
+
+# my_set = set({1,2,3,3,3,3,4,4,4,5})
+
+# print(my_set)
+
+
+# set_1 = {1,2,3,4,5}
+
+# set_2 = {3,5,7}
+
+# union_of_set = set_1.union(set_2)
+
+# intersection_of_set = set_1.intersection(set_2)
+
+# difference_of_set = set_1.difference(set_2)
+
+# print(difference_of_set)
+
+
+set_1 = {1,2,3,4,5}
+
+# set_1.add(10)
+
+# set_1.remove(2)
+
+# set_1.remove(20)
+
+# set_1.discard(5)
+# remvoing_elem = set_1.discard(15)
+
+# print(remvoing_elem)
+
+# print(set_1)
+
+
+
+# Frozenset --> immutable 
+
+froze_set_1 = frozenset({1,2,3,4})
+
+froze_set_2 = frozenset({1,2,3,4,9,10})
+
+union_of_froze_set = froze_set_1.union(froze_set_2)
+
+print(union_of_froze_set)
