bf6b239f0f9b3a917b8fbca2e53889fb16a48038
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-10-12 13:37:25 -0700

    loops in python (for loop full guide)

---
+++ loops.py
@@ -0,0 +1,89 @@
+# For Loop
+
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+# print("Hello World")
+
+
+# Loop through String
+# my_name = "Jawad"
+
+# for character in my_name:
+
+#     print(character)
+#     # print("Hello World")
+
+
+# Loop through a List
+
+# my_list = ["Jawad", "Khan", True, False, 242, 3.343]
+
+
+# for item in my_list:
+
+#     print(item)
+
+
+# user = {
+#     "name": "Jawad",
+#     "roll": "01003",
+#     "depart": "ai"
+# }
+
+# for key in user.keys():
+
+#     print(key)
+
+# for value in user.values():
+
+#     print(value)
+
+
+# for key, value in user.items():
+
+#     print(key, value)
+
+
+# Hello World 50 times
+
+
+# for number in range(0, 11, 5):
+
+#     print("Hello World", number)
+
+
+# Table of 5
+
+# table = 10
+
+# for count in range(1, 11):
+
+#     print(f"{count} X {table} = {count * table}")
+
+
+# Reverse A String using for loop
+
+# my_name = "Jawad"
+
+# reversed_str = ""
+
+# for char in my_name:
+
+#     reversed_str = char + reversed_str
+
+                
+# print(reversed_str)
+
+
+my_list = [2, 5, 7, 9, 11]
+
+for num in my_list:
+
+    if num % 2 == 0:
+        print(f"{num} is even")
+
+    else:
+         print(f"{num} is odd")
