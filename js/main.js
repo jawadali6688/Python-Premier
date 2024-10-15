e70128d3d3e44491a37d65fc3293d4fde46b14dc
jawadali6688 <jawaddotprogrammer@gmail.com>
2024-10-15 09:43:55 -0700

    some code of js

---
+++ js/main.js
@@ -0,0 +1,96 @@
+// Query Selector
+
+const heading = document.querySelector("h1")
+
+// const myHeading = document.querySelectorAll("h1")
+
+
+// console.log(heading)
+// console.log(myHeading[1])
+
+// setTimeout(() => {
+//     heading.innerText = "Hello Younis"
+// }, 2000);
+
+const myBox = document.getElementsByClassName("box")
+
+
+
+// setTimeout(() => {
+//     myBox[0].innerHTML = ` <h1>Hello World</h1>
+
+//     <h1>Hello Umar</h1>`
+// }, 2000);
+
+
+const myInput = document.getElementsByTagName("input")
+
+// const myAttr = myInput[0].getAttribute("value")
+
+
+// setTimeout(() => {
+//     const myAttr = myInput[0].setAttribute("value", "Register")
+// }, 2000);
+
+// const myAttr = myInput[0].getAttribute("value")
+
+// console.log(myAttr)
+
+// Toggle Classes
+
+
+
+// setTimeout(() => {
+//     myInput[0].classList.add("btn")
+//     myInput[0].setAttribute("value", "I love you ")
+// }, 3000);
+
+
+
+// const newInput = document.createElement("input")
+
+// setInterval(() => {
+//     newInput.setAttribute("type", "text")
+//     newInput.setAttribute("placeholder", "Enter Your Name")
+//     myBox[0].prepend(newInput)
+//     newInput.classList.toggle("coolInput")
+// }, 2000);
+
+
+
+
+
+
+
+// myInput[0].style.color = "pink"
+// myInput[0].style.backgroundColor = "blueviolet"
+
+
+
+// myInput[0].addEventListener("click", () => {
+//     alert("Hello my name is ")
+// })
+
+// addEventListener("click", function)
+
+// function sum(name) {
+//     alert("Hello my name is ", name)
+// }
+
+// sum("Jawad")
+
+
+
+// myInput[0].addEventListener("mouseleave", () => {
+//     document.getElementsByTagName("h1")[0].innerText = "Hello Umar"
+// })
+// myInput[0].addEventListener("mouseover", () => {
+//     document.getElementsByTagName("h1")[0].innerText = "Hello Younis"
+// })
+
+
+// console.log(myInput[0])
+// myInput[0].addEventListener("change", (input) => {
+//     console.log(input.target.value)
+//     document.getElementsByTagName("h1")[0].innerText = "Hello Younis"
+// })
